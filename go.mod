1c568eaca1b8ed95bbe5a86b2b346a2c2bc78b37
Morlay <morlay.null@gmail.com>
2018-06-28 12:37:28 +0800

    should err

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/morlay/vgo-require-local-first
+
+require github.com/go-courier/loaderx v0.0.0-20180627072405-a228eebb5123
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"github.com/go-courier/loaderx"
+	"github.com/stretchr/testify/assert"
+)
+
+func main() {
+	_ = loaderx.Load
+	_ = assert.Equal
+}
